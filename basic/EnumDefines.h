6bfe72e289e620ef22378f6ed3104ec01a0aad62
skymelody <lzfgnu@gmail.com>
2021-02-15 22:24:06 +0800

    [update] update EnumDefines.h

--- basic/EnumDefines.h
+++ basic/EnumDefines.h
@@ -2,50 +2,96 @@
 #include <mutex>
 #include <sstream>
 #include <vector>
+#include <map>
 
-#define DefineEnums(EnumClassName, EnumArgs...)                           \
-  enum class EnumClassName {                                              \
-    EnumArgs                                                              \
-  };                                                                      \
-  inline std::string Str##EnumClassName(EnumClassName enumVal) {          \
-    static bool inited{ false };                                          \
-    static int offset{ 0 };                                               \
+#define DefineEnums(EnumClassName, EnumArgs...)                                     \
+  enum class EnumClassName {                                                        \
+    EnumArgs                                                                        \
+  };                                                                                \
+  inline std::string EnumClassName##2Str(EnumClassName enumVal) {                   \
+    static bool inited{ false };                                                    \
+    static int offset{ 0 };                                                         \
     /*
-     * to reduce space consume, we can                                    
+     * to reduce space consume, we can                                              
      * make enumArgsStr pointer                                   
-     * after init finished, delete it                                     
-     */                                                                   \
-    static std::mutex mtx;                                                \
-    static std::string enumArgsStr = #EnumArgs;                           \
-    static std::vector<std::string> enumStrV;                             \
-                                                                          \
-    if (!inited) {                                                        \
-      std::lock_guard<std::mutex> lock(mtx);                              \
-      if (!inited) {                                                      \
-        std::istringstream iss(enumArgsStr);                              \
-        std::string enumStr;                                              \
-        bool first = true;                                                \
-                                                                          \
-        while (std::getline(iss, enumStr, ',')) {                         \
-          /* TODO: trim str */                                            \
-          int pos = enumStr.find('=');                                    \
-          if (first) {                                                    \
-            first = false;                                                \
-            if (pos != std::string::npos) {                               \
-              offset = std::stoi(enumStr.substr(pos+1));                  \
-            }                                                             \
-          }                                                               \
-          if (pos != std::string::npos) {                                 \
-            enumStrV.push_back(enumStr.substr(0, pos));                   \
-          }                                                               \
-          else {                                                          \
-            enumStrV.push_back(enumStr);                                  \
-          }                                                               \
-        }                                                                 \
-      }                                                                   \
-      inited = true;                                                      \
-    }                                                                     \
-                                                                          \
-    int pos = static_cast<int>(enumVal) - offset;                         \
-    return (pos >= enumStrV.size() || pos < 0) ? "" : enumStrV[pos];      \
-  }
+     * after init finished, delete it                                               
+     */                                                                             \
+    static std::mutex mtx;                                                          \
+    static std::string enumArgsStr = #EnumArgs;                                     \
+    static std::vector<std::string> enumStrV;                                       \
+                                                                                    \
+    if (!inited) {                                                                  \
+      std::lock_guard<std::mutex> lock(mtx);                                        \
+      if (!inited) {                                                                \
+        std::istringstream iss(enumArgsStr);                                        \
+        std::string enumStr;                                                        \
+        bool first = true;                                                          \
+                                                                                    \
+        while (std::getline(iss, enumStr, ',')) {                                   \
+          /* TODO: trim str */                                                      \
+          int pos = enumStr.find('=');                                              \
+          if (first) {                                                              \
+            first = false;                                                          \
+            if (pos != std::string::npos) {                                         \
+              offset = std::stoi(enumStr.substr(pos+1));                            \
+            }                                                                       \
+          }                                                                         \
+          if (pos != std::string::npos) {                                           \
+            enumStrV.push_back(enumStr.substr(0, pos));                             \
+          }                                                                         \
+          else {                                                                    \
+            enumStrV.push_back(enumStr);                                            \
+          }                                                                         \
+        }                                                                           \
+      }                                                                             \
+      inited = true;                                                                \
+    }                                                                               \
+                                                                                    \
+    int pos = static_cast<int>(enumVal) - offset;                                   \
+    return (pos >= enumStrV.size() || pos < 0) ? "" : enumStrV[pos];                \
+  }                                                                                 \
+                                                                                    \
+  struct Str##EnumClassName {                                                       \
+    Str##EnumClassName(EnumClassName enumVal) : val(enumVal) {                      \
+      if (!inited) {                                                                \
+        std::lock_guard<std::mutex> lock(mtx);                                      \
+        if (!inited) {                                                              \
+          std::istringstream iss(*enumArgsStr);                                     \
+          std::string enumStr;                                                      \
+          int offset = 0;                                                           \
+                                                                                    \
+          while (std::getline(iss, enumStr, ',')) {                                 \
+            /* TODO: trim str */                                                    \
+            int pos = enumStr.find('=');                                            \
+            if (pos != std::string::npos) {                                         \
+              offset = std::stoi(enumStr.substr(pos+1));                            \
+            }                                                                       \
+            enumMap[offset] = enumStr.substr(0, pos);                               \
+            ++offset;                                                               \
+          }                                                                         \
+          delete enumArgsStr;                                                       \
+          enumArgsStr = nullptr;                                                    \
+          inited = true;                                                            \
+        }                                                                           \
+      }                                                                             \
+    }                                                                               \
+                                                                                    \
+    std::string Str() {                                                             \
+      int tmp = int(val);                                                           \
+      return enumMap.count(tmp) ?                                                   \
+        enumMap[tmp] : "";                                                          \
+    }                                                                               \
+                                                                                    \
+  private:                                                                          \
+    EnumClassName val;                                                              \
+                                                                                    \
+  private:                                                                          \
+    static bool inited;                                                             \
+    static std::mutex mtx;                                                          \
+    static std::string* enumArgsStr;                                                \
+    static std::map<int, std::string> enumMap;                                      \
+  };                                                                                \
+  bool Str##EnumClassName::inited = false;                                          \
+  std::mutex Str##EnumClassName::mtx;                                               \
+  std::string* Str##EnumClassName::enumArgsStr = new std::string(#EnumArgs);        \
+  std::map<int, std::string> Str##EnumClassName::enumMap; 
--- basic/test/TestEnumDefines.cc
+++ basic/test/TestEnumDefines.cc
@@ -1,5 +1,7 @@
 #include "../EnumDefines.h"
 #include <iostream>
+#include <thread>
+#include <vector>
 
 #define X 1
 const int Y = 2;
@@ -14,13 +16,39 @@ DefineEnums(F, x=50505, y, z);
 // not working for G
 DefineEnums(G, a=Y, b, c);
 
-int main() {
-  std::cout << StrA(A::a) << std::endl;
-  std::cout << StrB(B::a) << " " << StrB(B::b) << " " << StrB(B::c) << std::endl;
-  std::cout << StrB(B::a) << " " << StrB(B::b) << " " << StrB(B::c) << std::endl;
-  std::cout << StrC(C::a) << " " << StrC(C::b) << " " << StrC(C::c) << std::endl;
-  std::cout << StrD(D::a) << " " << StrD(D::b) << " " << StrD(D::c) << std::endl;
-  std::cout << StrE(E::a) << " " << StrE(E::b) << " " << StrE(E::c) << std::endl;
-  std::cout << StrF(F::x) << " " << StrF(F::y) << " " << StrF(F::z) << std::endl;
+void TestEnum2Str() {
+  std::cout << A2Str(A::a) << std::endl;
+  std::cout << B2Str(B::a) << " " << B2Str(B::b) << " " << B2Str(B::c) << std::endl;
+  std::cout << B2Str(B::a) << " " << B2Str(B::b) << " " << B2Str(B::c) << std::endl;
+  std::cout << C2Str(C::a) << " " << C2Str(C::b) << " " << C2Str(C::c) << std::endl;
+  std::cout << D2Str(D::a) << " " << D2Str(D::b) << " " << D2Str(D::c) << std::endl;
+  std::cout << E2Str(E::a) << " " << E2Str(E::b) << " " << E2Str(E::c) << std::endl;
+  std::cout << F2Str(F::x) << " " << F2Str(F::y) << " " << F2Str(F::z) << std::endl;
   // std::cout << StrG(G::a) << " " << StrG(G::b) << " " << StrG(G::c) << std::endl;
+
+  /*
+  std::cout << StrA(A::a).Str() << std::endl;
+  std::cout << StrB(B::a).Str() << " " << StrB(B::b).Str() << " " << StrB(B::c).Str() << std::endl;
+  std::cout << StrB(B::a).Str() << " " << StrB(B::b).Str() << " " << StrB(B::c).Str() << std::endl;
+  std::cout << StrC(C::a).Str() << " " << StrC(C::b).Str() << " " << StrC(C::c).Str() << std::endl;
+  std::cout << StrD(D::a).Str() << " " << StrD(D::b).Str() << " " << StrD(D::c).Str() << std::endl;
+  std::cout << StrE(E::a).Str() << " " << StrE(E::b).Str() << " " << StrE(E::c).Str() << std::endl;
+  std::cout << StrF(F::x).Str() << " " << StrF(F::y).Str() << " " << StrF(F::z).Str() << std::endl;
+  */
+}
+
+void TestMultiThread() {
+  std::vector<std::thread> v;
+  for (int i = 0; i < 10; ++i) {
+    v.push_back(std::thread([] {
+      TestEnum2Str();
+    }));
+  }
+  for (int i = 0; i < 10; ++i) {
+    v[i].join();
+  }
+}
+
+int main() {
+  TestEnum2Str();
 }
